cc37591d31763febdeb6d202e4235dea868b401f
Denis Salmanovich <dsalmanovich@eisgroup.com>
2024-02-21 11:44:49 +0200

    v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,72 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use egui_macroquad::*;
-use macroquad::prelude::*;
+use std::time::{SystemTime, UNIX_EPOCH};
+use eframe::egui;
 use denlibs::random::{Direction, random_direction};
 use mouse_rs::Mouse;
 
-const DELAY: f64 = 5.0;
+const DELAY: u128 = 5000;
 
-fn conf() -> Conf {
-    let mut title = String::from("Mouse mover by -=DeN=- v");
-    title.push_str(env!("CARGO_PKG_VERSION"));
-    Conf {
-        window_title: title
-        .to_owned(),
-        fullscreen: false,
-        sample_count: 16,
-        window_width: 350,
-        window_height: 90,
-        window_resizable: false,
-        ..Default::default()
-    }
+fn get_time() -> u128 {
+    SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis()
 }
 
-#[macroquad::main(conf)]
-async fn main() {
-    let mouse = Mouse::new();
-    let mut time_now: f64 = get_time();
-    
-    loop {
-        clear_background(BLACK);
+fn load_icon() -> eframe::IconData {
+    eframe::IconData::try_from_png_bytes(&include_bytes!("../assets/icon.png")[..]).unwrap()
+}
 
-        egui_macroquad::ui(|ctx| {
-            egui::CentralPanel::default().show(&ctx, |ui| {
+fn main() {
+    let mut time_now = get_time();
+    let mut active = true;
+    let mouse = Mouse::new();
+    
+    let mut title = String::from("Mouse mover v");
+    title.push_str(env!("CARGO_PKG_VERSION"));
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(300.0, 80.0)),
+        resizable: false,
+        icon_data: Some(load_icon()),
+        ..Default::default()
+    };
+
+    eframe::run_simple_native(&title, options, move |ctx, _frame| {
+        egui::CentralPanel::default().show(&ctx, |ui| {
+            ui.horizontal(|ui| {
+                ui.checkbox(&mut active, "ativated");
+            });
+            ui.separator();
+            ui.with_layout(egui::Layout::right_to_left(egui::Align::TOP), |ui| {
                 if ui.button(egui::RichText::new("Quit").color(egui::Color32::RED)).clicked() {
                     std::process::exit(0x0100);
                 }
             });
-        });
+           
+            ui.horizontal_wrapped(|ui| {
+                ui.label(egui::RichText::new("By -=DeN=-").color(egui::Color32::WHITE));
+            });
 
-        egui_macroquad::draw();
+            if active {
+                if get_time() - time_now > DELAY {
+                    let cur_mouse_position = mouse_position::mouse_position::Mouse::get_mouse_position();
 
-        if get_time() - time_now > DELAY {
-            let cur_mouse_position = mouse_position::mouse_position::Mouse::get_mouse_position();
-            
-            match cur_mouse_position {
-                mouse_position::mouse_position::Mouse::Position { mut x, mut y } => {
-                    match random_direction().unwrap() {
-                        Direction::Up => {y -= 1},
-                        Direction::Down => {y += 1},
-                        Direction::Left => {x -= 1},
-                        Direction::Right => {x += 1},
-                    };
-                    mouse.move_to(x+1, y+1).expect("Unable to move mouse");
-                },
-                mouse_position::mouse_position::Mouse::Error => println!("Error getting mouse position"),
+                    match cur_mouse_position {
+                        mouse_position::mouse_position::Mouse::Position { mut x, mut y } => {
+                            match random_direction().unwrap() {
+                                Direction::Up => {y -= 1},
+                                Direction::Down => {y += 1},
+                                Direction::Left => {x -= 1},
+                                Direction::Right => {x += 1},
+                            };
+                            mouse.move_to(x, y).expect("Unable to move mouse");
+                        },
+                        mouse_position::mouse_position::Mouse::Error => println!("Error getting mouse position"),
+                    }
+
+                    time_now = get_time();
+                }
             }
-
-            time_now = get_time();
-        }
-
-        next_frame().await
-    }
+        });
+        
+        ctx.request_repaint();
+    }).unwrap();
 }
-
